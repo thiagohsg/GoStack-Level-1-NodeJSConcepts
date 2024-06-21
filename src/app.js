9e8d9b8ae33ccaebb126ffd41c9f1e4e8320e195
Thiago Gouvea <thiagogouvea@gmail.com>
2024-06-20 23:55:08 -0300

    Middleware creation and list filter

--- src/app.js
+++ src/app.js
@@ -5,15 +5,64 @@ const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
+// Função que todas as rotas passam por ela para o express interpretar o JSON.
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  //const params = request.query;
+function logRequests(request, response, next) {
+  const { method, url } = request;
+  const logLabel = `${new Date().toISOString()} - [${method.toUpperCase()}] ${url}`;
 
-  return response.json(repositories);
+  console.time(logLabel);
+
+  // Chama o próximo middleware para não interromper a requisição
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function logRequestsStatus(request, response, next) {
+  const { method, url } = request;
+  const start = Date.now();
+
+  response.on('finish', () => {
+    const duration = Date.now() - start;
+    const { statusCode } = response;
+    const logMessage = `${new Date().toISOString()} - [${method.toUpperCase()}] ${statusCode} ${url} - ${duration}ms`;
+    console.log(logMessage);
+  });
+
+  // Chama o próximo middleware para não interromper a requisição
+  next();
+}
+
+
+function validateRepositoryID(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ erro: 'Invalid repository ID.' });
+  }
+
+  return next();
+}
+
+// app.use(logRequests);
+app.use(logRequestsStatus);
+
+// especificando as rotas que vão utilzar o middleware
+app.use('/repositories/:id', validateRepositoryID);
+
+app.get("/repositories", (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? repositories.filter(repository => repository.title.includes(title))
+    : repositories;
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
@@ -30,6 +79,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
+  // Procuro a posição do projeto no vetor findIndex
   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
